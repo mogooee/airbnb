20e392a1c0e04bb227619b8ca6aaec19eb9bf8aa
Song TaeHwan <axoghks@gmail.com>
2022-05-23 11:52:57 +0900

    chore: iOS 프로젝트 세팅

---
+++ ios/AirBnb/AirBnb/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AirBnb
+//
+//  Created by 송태환 on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.backgroundColor = .white
+	}
+
+}
+
