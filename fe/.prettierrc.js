7daf7adf0373f03aa75d95ca9656457b4500545e
연 <mogoo22@naver.com>
2022-05-23 23:59:53 +0900

    chore: airbnb eslint, prettier

---
+++ fe/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    singleQuote: true,
+    semi: true,
+    tabWidth: 2,
+    bracketSpacing: true,
+    proseWrap: 'never',
+    trailingComma: 'all',
+    useTabs: false,
+    arrowParens: 'avoid',
+    endOfLine: 'auto',
+};
