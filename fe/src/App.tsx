652d784d96fa114ebbc69579e33c4a988818848b
연 <mogoo22@naver.com>
2022-05-25 20:13:25 +0900

    design:  GlobaslStyles로 styled-reset 적용

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Routers from 'components/Routers/Routers';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle`
+    ${reset};
+`;
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Routers />
     </div>
   );
