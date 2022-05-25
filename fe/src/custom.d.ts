4b13eaac18654d4d129d63b287f26fca0dab62c4
연 <mogoo22@naver.com>
2022-05-25 22:00:16 +0900

    update: hero-img.png

--- fe/src/custom.d.ts
+++ fe/src/custom.d.ts
@@ -1,3 +1,11 @@
+declare module '*.png' {
+  import React = require('react');
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGSSVGElement>>;
+  const src: string;
+  export default src;
+}
+
 declare module '*.svg' {
   import React = require('react');
 
