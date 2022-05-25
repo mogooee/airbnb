03c01f40073a0ad486cb9ae60d9d3e1e1c74e663
연 <mogoo22@naver.com>
2022-05-25 12:28:01 +0900

    feat: main, search, notFound page 라우터 적용

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,12 @@
+import Routers from 'components/Routers/Routers';
 import React from 'react';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Routers />
+    </div>
+  );
 }
 
 export default App;
