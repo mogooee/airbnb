1c464aca652e4d56e89e32489a0abfefd27eaa07
tokaji <fpzk5656@gmail.com>
2022-05-27 13:47:08 +0900

    feat: service ,Repository ,Controller 추가

--- be/src/main/java/teamproject/airbnb/repository/MemberRepository.java
+++ be/src/main/java/teamproject/airbnb/repository/MemberRepository.java
@@ -1,15 +1,16 @@
 package teamproject.airbnb.repository;
 
+import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import teamproject.airbnb.domain.Member;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-	@PersistenceContext
-	private EntityManager em;
+	private final EntityManager em;
 
 	public void save(Member member) {
 		em.persist(member);
@@ -17,5 +18,11 @@ public class MemberRepository {
 
 	public Member findOne(Long id) {
 		return em.find(Member.class, id);
+	}
+
+	public List<Member> findByName(String name) {
+		return em.createQuery("select m from Member m where m.name = :name", Member.class)
+			.setParameter("name", name)
+			.getResultList();
 	}
 }
