072ed53ceef2ff7433085858acde344abbd04951
연 <mogoo22@naver.com>
2022-06-01 18:42:34 +0900

    feat: DeleteButton을 누르면 해당하는 여역의 값을 초기화 한다.

--- fe/src/components/SearchBar/Personnel/Personnel.tsx
+++ fe/src/components/SearchBar/Personnel/Personnel.tsx
@@ -5,7 +5,7 @@ import { SectionProps } from 'components/SearchBar/types';
 export default function Personnel({ search, addSearch }: SectionProps) {
   const { title, defaultValue, value } = search;
   const { adult, teenager } = value;
-  const guest = (adult as number) + (teenager as number);
+  const guest = (adult || 0) + (teenager || 0);
 
   return <SearchSection title={title as string} value={guest || defaultValue} />;
 }
