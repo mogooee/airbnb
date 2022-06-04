21b2b2975ea4d960c9a7cb48d9f10da3c166ea7c
tokaji <fpzk5656@gmail.com>
2022-06-05 00:42:34 +0900

    refactor: RoomQuantityResponseDto 에서 문구 없이 데이터만 반환하도록 변경

--- be/src/main/java/teamproject/airbnb/controller/RoomQuantityResponseDto.java
+++ be/src/main/java/teamproject/airbnb/controller/RoomQuantityResponseDto.java
@@ -9,8 +9,9 @@ import teamproject.airbnb.service.RoomQuantityDto;
 @AllArgsConstructor
 public class RoomQuantityResponseDto {
 
-	private String strPriceRange;
-	private String strAveragePrice;
+	private String minimumFormat;
+	private String maximumFormat;
+	private String averageFormat;
 	private List<Long> priceRange;
 	private List<Long> roomQuantity;
 
@@ -20,11 +21,7 @@ public class RoomQuantityResponseDto {
 		String maximumFormat = roomQuantityDto.getMaximumPriceFormat();
 		String averageFormat = roomQuantityDto.getAveragePriceFormat();
 
-		String strPriceRange = "\\" + minimumFormat + "-" + maximumFormat + "+";
-		String strAveragePrice = "평균 1박 요금은 \\" + averageFormat + "입니다.";
-
-		return new RoomQuantityResponseDto(strPriceRange, strAveragePrice,
-			roomQuantityDto.getPriceRange(),
-			roomQuantityDto.getRoomQuantity());
+		return new RoomQuantityResponseDto(minimumFormat, maximumFormat, averageFormat,
+			roomQuantityDto.getPriceRange(), roomQuantityDto.getRoomQuantity());
 	}
 }
