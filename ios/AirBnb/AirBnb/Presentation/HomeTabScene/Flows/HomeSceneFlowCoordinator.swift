b63aeae0e0645c2122a151d3a9f7cf14c676d8f1
Song TaeHwan <axoghks@gmail.com>
2022-06-09 16:13:56 +0900

    feat: HomeSceneFlowCoordinator 구현

---
+++ ios/AirBnb/AirBnb/Presentation/HomeTabScene/Flows/HomeSceneFlowCoordinator.swift
@@ -0,0 +1,32 @@
+//
+//  HomeTabSceneCoordinator.swift
+//  AirBnb
+//
+//  Created by 송태환 on 2022/06/07.
+//
+
+import UIKit
+
+protocol HomeSceneFlowCoordinatorDependencies {
+  func makeHomeViewController() -> HomeViewController
+  func makeLocationSearchViewController() -> LocationSearchController
+}
+
+final class HomeSceneFlowCoordinator: Coordinator {
+  private let navigationController: UINavigationController
+  private let dependencies: HomeSceneFlowCoordinatorDependencies
+
+  init(navigationController: UINavigationController, dependencies: HomeSceneFlowCoordinatorDependencies) {
+    self.navigationController = navigationController
+    self.dependencies = dependencies
+  }
+
+  func start() {
+    let viewController = dependencies.makeHomeViewController()
+    navigationController.viewControllers = [viewController]
+  }
+
+  func showSearchLocation() {
+    let viewController = dependencies.makeLocationSearchViewController()
+  }
+}
