c842e662f529beb5ee4d73b79b93658a1426e95c
Song TaeHwan <axoghks@gmail.com>
2022-06-09 16:16:05 +0900

    refactor: SceneDelegate 리펙토링

--- ios/AirBnb/AirBnb/Application/SceneDelegate.swift
+++ ios/AirBnb/AirBnb/Application/SceneDelegate.swift
@@ -9,46 +9,20 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
+  var coordinator: AppCoordinator?
 
   func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
     guard let scene = (scene as? UIWindowScene) else {
       return
     }
 
-    let tabBarController = UITabBarController()
-    let homeController = HomeViewController.create(with: DefaultHomeViewModel())
-    let wishListController = WishListViewController()
-    let reservationController = ReservationViewController()
-
-    homeController.tabBarItem = .init(
-      title: "검색",
-      image: .magnifier,
-      selectedImage: .magnifier
-    )
-
-    wishListController.tabBarItem = .init(
-      title: "위시리스트",
-      image: .heart,
-      selectedImage: .heartFilled
-    )
-
-    reservationController.tabBarItem = .init(
-      title: "예약",
-      image: .profile,
-      selectedImage: .profileFilled
-    )
-
-    tabBarController.viewControllers = [
-      UINavigationController(rootViewController: homeController),
-      UINavigationController(rootViewController: wishListController),
-      reservationController
-    ]
-
-    tabBarController.tabBar.backgroundColor = .gray6
-    tabBarController.tabBar.addTopBorder(with: .gray4, andWidth: 0.5)
+    let navigationController = UINavigationController()
 
     window = UIWindow(windowScene: scene)
-    window?.rootViewController = tabBarController
+    window?.rootViewController = navigationController
     window?.makeKeyAndVisible()
+
+    coordinator = AppCoordinator(navigationController: navigationController, container: AppDIContainer())
+    coordinator?.start()
   }
 }
